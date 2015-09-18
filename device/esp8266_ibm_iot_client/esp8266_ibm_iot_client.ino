e40c1c7610207f579ca5b884b7cf9aee02d836e9
Nathan Friedly <nathan@nfriedly.com>
2015-09-18 18:28:24 -0400

    correcting device properties

--- device/esp8266_ibm_iot_client/esp8266_ibm_iot_client.ino
+++ device/esp8266_ibm_iot_client/esp8266_ibm_iot_client.ino
@@ -1,40 +1,20 @@
 /*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
+IBM IOT Foundation Quickstart for ESP8266
+More info at https://github.com/watson-developer-cloud/stt-iot-disrupt-2015
 */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
+const char* ssid = "<YOUR WIFI SSID>";
+const char* wifi_password = "<YOUR WIFI PASSWORD>";
 
-const char* ssid = "Friedly";
-const char* wifi_password = "1234554321";
-
-const char* username = "use-token-auth"; // optional. Set to "use-token-auth" for token authentication
-const char* password = "w4E+mos+RI)Xo5z45x"; // optional. Token goes here for token auth.
-const char* clientId = "d:6b4nht:esp8266:18FE34EF211C"; // Device ID. change second part to orgId and last part to mac address - WiFi.macAddress() but without the colons 
-const char* mqtt_server = "6b4nht.messaging.internetofthings.ibmcloud.com"; // create a service instance in bluemix, bind to an aopp, click show credentials
+// for the quickstart server, a lot of this can be null or just "quickstart"
+// for a private instance, create an IoT service instance in Iluemix, bind to an app, click show credentials
+const char* username = null; // Set to null for Quickstart or "use-token-auth" for token authentication
+const char* password = null; // optional. Token goes here for token auth.
+const char* clientId = "d:<YOUR ORG ID OR quickstart>:esp8266:<YOUR DEVICE'S MAC ADDRESS>"; // change second part to orgId and last part to mac address - WiFi.macAddress() but without the colons
+const char* mqtt_server = "<YOUR ORG ID OR quickstart>.messaging.internetofthings.ibmcloud.com";
 
 
 WiFiClient espClient;
@@ -44,12 +24,9 @@ char msg[50];
 int value = 0;
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output - this may not exist on some ESP8266 devices, so change if necessary
   Serial.begin(115200);
-
-
   setup_wifi();
-  
   Serial.print("server: "); Serial.println(mqtt_server);
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
